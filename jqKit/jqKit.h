c358a10b141044e0303ec6f796e78d430214a23b
Micha Mazaheri <micha@mazaheri.me>
2019-08-26 19:40:11 +0300

    Initial commit

---
+++ jqKit/jqKit.h
@@ -0,0 +1,19 @@
+//
+//  jqKit.h
+//  jqKit
+//
+//  Created by Micha Mazaheri on 8/26/19.
+//  Copyright © 2019 Paw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for jqKit.
+FOUNDATION_EXPORT double jqKitVersionNumber;
+
+//! Project version string for jqKit.
+FOUNDATION_EXPORT const unsigned char jqKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jqKit/PublicHeader.h>
+
+
---
+++ jqKitTests/jqKitTests.m
@@ -0,0 +1,37 @@
+//
+//  jqKitTests.m
+//  jqKitTests
+//
+//  Created by Micha Mazaheri on 8/26/19.
+//  Copyright © 2019 Paw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface jqKitTests : XCTestCase
+
+@end
+
+@implementation jqKitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
